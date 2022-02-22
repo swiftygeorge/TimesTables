6ca68fd81189dc304e281a0b6669d4d1326c9b82
George Mapaya <georit01@icloud.com>
2022-02-22 10:34:15 -0500

    Initial Commit

---
+++ TimesTables/TimesTablesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimesTablesApp.swift
+//  TimesTables
+//
+//  Created by George Mapaya on 2022-02-22.
+//
+
+import SwiftUI
+
+@main
+struct TimesTablesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
